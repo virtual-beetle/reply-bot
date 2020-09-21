bdab8460a8a903e8e4806afd9a189972ae93ff2d
eshan-b <eshan.bharadwaj04@gmail.com>
2020-09-21 17:12:06 -0500

    added game code functionality

--- index.js
+++ index.js
@@ -1,37 +1,70 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
 const token = process.env.BOT_TOKEN;
 
-client.once('ready', () => {
-    console.log('Ready!');
+client.once("ready", () => {
+  console.log("Ready!");
 });
 
-client.on('message', async message => {
-    if (message.content.includes('bruh')) {
-        try {
-            await message.react('🇧');
-            await message.react('🇷');
-            await message.react('🇺');
-            await message.react('🇭');
-        } catch (error) {
-            console.error('One of the emojis failed to react.');
+client.on("message", async (message) => {
+  if (message.content.includes("bruh")) {
+    try {
+      await message.react("🇧");
+      await message.react("🇷");
+      await message.react("🇺");
+      await message.react("🇭");
+    } catch (error) {
+      console.error("One of the emojis failed to react.");
+    }
+  }
+
+  if (message.content.includes("lmao")) {
+    message.react("😂");
+  }
+
+  if (message.content.includes("oof")) {
+    message.react("753386121123201164");
+  }
+
+  if (message.content.includes("rip")) {
+    message.react("754217664808878141");
+  }
+
+  if (
+    message.channel == "757708878203977888" &&
+    message.content == "oof among_us"
+  ) {
+    var targetMember = message.get(message.author);
+    message.reply("What is the Among Us code?...");
+
+    message.channel
+      .awaitMessages((m) => m.author.id == message.author.id, {
+        max: 1,
+        time: 30000,
+      })
+      .then((collected) => {
+        var gameCode = collected.first().content;
+        if (collected.first().content.length() <= 6) {
+          const embedMsg = new Discord.MessageEmbed()
+            .setTitle("Among Us Game")
+            .setDescription(
+              targetMember +
+                "hosted an Among Us Game\nJoin the game: `" +
+                gameCode +
+                "`"
+            )
+            .setTimestamp();
+        } else {
+          message.reply("That is not a valid code");
         }
-    }
-
-    if (message.content.includes('lmao')) {
-        message.react('😂');
-    }
-
-    if (message.content.includes('oof')) {
-        message.react('753386121123201164');
-    }
-
-    if (message.content.includes('rip')) {
-        message.react('754217664808878141');
-    }
+      })
+      .catch(() => {
+        message.reply("No answer after 30 seconds, operation canceled.");
+      });
+  }
 });
 
 client.login(token);
